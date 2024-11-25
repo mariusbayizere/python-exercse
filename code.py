a9fd5f34005e181d32c58fbb438b12543fdf98e0
Bayizere Marius <bayizeremarius119@gmail.com>
2024-11-25 10:40:39 +0200

    first commit

---
+++ code.py
@@ -0,0 +1,19 @@
+
+def sum(x: int, y: int) -> int:
+    """
+    Returns the sum of two parameters.
+
+    
+    Parameters:
+    x (int): one number
+    y (int): another number
+
+    
+    Returns:
+    int: the sum of x and y
+    """
+    print("x is", x)
+    print("y is", y)
+    return x + y
+
+print(sum(3, 4))
