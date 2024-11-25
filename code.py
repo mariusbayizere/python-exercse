c20cde49d4dd87bedf651df020c76073fc386e52
Bayizere Marius <bayizeremarius119@gmail.com>
2024-11-25 17:27:14 +0200

    thrid-message

--- code.py
+++ code.py
@@ -3,8 +3,9 @@ import matplotlib.pyplot as plt
 x_values: list[int] = [1, 2, 3, 4, 5]
 y_values: list[int] = [2, 3, 5, 7, 11]
 
-plt.plot(x_values, y_values)
+
+plt.bar(x_values, y_values)
 plt.title('This chat show as the information about the chat')
-plt.xlabel('X')
-plt.ylabel('Y')
+plt.xlabel('Category X')
+plt.ylabel('Category Y')
 plt.show()
